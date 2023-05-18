503a754a0dfae5df43ea7d38b36a689f88a62f7a
MoKhatib2 <121944981+MoKhatib2@users.noreply.github.com>
2023-05-18 13:28:48 +0200

    Delete src/network directory

--- src/network/GameServer.java
+++
@@ -1,9 +0,0 @@
-package network;
-
-public class GameServer {
-	public static boolean running = false;
-	public static void main(String args[ ]) {
-		System.out.println("Game server up and running...");
-		if (!running) new GameDaemon().start();
-	}
-}
